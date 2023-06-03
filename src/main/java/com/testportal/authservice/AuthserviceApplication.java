18fb7235f7d6659e9848053e9cafd6ff11712276
cenajhonakash@gmail.com <cenajhonakash@gmail.com>
2023-06-03 14:34:42 +0530

    fix: Removed oauth2 security configurations from main application

--- src/main/java/com/testportal/authservice/AuthserviceApplication.java
+++ src/main/java/com/testportal/authservice/AuthserviceApplication.java
@@ -3,13 +3,11 @@ package com.testportal.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableAuthorizationServer
-@EnableResourceServer
+//@EnableAuthorizationServer
+//@EnableResourceServer
 public class AuthserviceApplication {
 
 	public static void main(String[] args) {
